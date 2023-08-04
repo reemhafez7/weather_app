0c3bd7b4b47f7fd575545649820d03c97f8f2cf6
reemhafez7 <88289261+reemhafez7@users.noreply.github.com>
2023-08-04 23:54:07 +0300

    Delete pokedex-main directory

--- pokedex-main/test/domain/entities/pokemon_detail_entity_mock.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pokedex/domain/entities/pokemon_detail_entity.dart';
-
-import '../../data/models/pokemon_detail_model_mock.dart';
-
-PokemonDetailEntity mockPokemonDetailEntity =
-    mockPokemonDetailModel.convertToEntity();
--- pokedex-main/test/domain/entities/pokemon_response_entity_mock.dart
+++
@@ -1,8 +0,0 @@
-import 'package:pokedex/domain/entities/pokemon_response_entity.dart';
-
-import 'pokemon_entity_mock.dart';
-
-const PokemonResponseEntity mockPokemonResponseEntity = PokemonResponseEntity(
-  next: 'https://pokeapi.co/api/v2/pokemon?offset=10&limit=10',
-  pokemons: mockPokemonEntityList,
-);
